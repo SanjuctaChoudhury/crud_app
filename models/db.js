b65fdcfd0a90814d69941a66538687111d71846f
sanjucta <csanjucta@gmail.com>
2021-07-18 09:37:26 +0530

    initial

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 require("./employee_model")
-mongoose.connect('mongodb://localhost:27017/employeeDB',{ useNewUrlParser: true , useUnifiedTopology: true,useFindAndModify:false },(err)=>
+mongoose.connect('process.env.MONGODB_URL||mongodb://localhost:27017/employeeDB',{ useNewUrlParser: true , useUnifiedTopology: true,useFindAndModify:false },(err)=>
 {
     if(!err)
     console.log("connection successfull")
